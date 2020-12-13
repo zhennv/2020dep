b42e09b3e2476c2dec1b1fcde88e287807ea5395
aetny <2094251034@qq.com>
2020-12-13 16:57:50 +0800

    ce'shi

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ import '@/assets/styles/layout.less'
 import axios from './providers/axios.js'
 import api from './providers/api.js'
 //组件
-
 import components from './components/index.js'
 Vue.prototype.$axios = axios;
 Vue.prototype.$api = api;
