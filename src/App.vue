45e16de8df77909f3cc7b1be2a2ff396391ad221
zhennv <2094251034@qq.com>
2020-12-13 14:56:09 +0800

    去掉变量没调用的校验警告，对主体部分样式的修改

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="app-frame-out">
+    <!-- 将公共的头部和尾部提取出来 使用vuex管理状态 -->
+    <!-- header start -->
     <headerTop v-show="showHead" ref="header"></headerTop>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-    <footerBar></footerBar>
-    <!--     
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- header end -->
+
+    <!-- content start -->
+    <div class="content">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
     </div>
-    <router-view/> -->
+    <!-- content end -->
+
+    <!-- footer start -->
+    <footerBar></footerBar>
+    <!-- footer end -->
+
+    <!-- 
+      分别在组件中写页面的 头部和尾部，方便交互
+      <div id="nav">
+        <router-link to="/">Home</router-link> 
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/> 
+    -->
   </div>
 </template>
 <script>
@@ -22,17 +36,17 @@ export default {
     };
   },
   provide() {
+    //写在最外层的组件中 方便传递到更深嵌套的组件里 但是有参数传递的难题
     return {
-      fooMethod: () => {
+      fooMethod: (fn) => {
         this.$refs.header.onClickRight(fn);
-        console.log("父组件 fooMethod被调用");
       },
     };
   },
   watch: {
+    //监听路由变化 实现部分页面头部和尾部的显示隐藏
     $route: {
-      handler(newVal, oldVal) {
-        console.log(newVal);
+      handler(newVal) {
         if (this.noHeads.includes(newVal.name)) {
           this.showHead = false;
         } else {
@@ -44,6 +58,7 @@ export default {
     },
   },
   created() {
+    //进入页面时判断 恢复vuex管理状态到刷新之前
     if (sessionStorage.getItem("store")) {
       this.$store.replaceState(
         Object.assign(
@@ -53,7 +68,10 @@ export default {
         )
       );
     }
+    //在浏览器窗口关闭或刷新时 会触发beforeunload事件
     window.addEventListener("beforeunload", () => {
+      window.alert("111");
+      console.log("111");
       sessionStorage.setItem("store", JSON.stringify(this.$store.state));
     });
   },
@@ -75,6 +93,11 @@ export default {
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+.app-frame-out {
+  .content {
+    flex: 1;
   }
 }
 </style>
