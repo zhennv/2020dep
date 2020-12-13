94a0524c647ba826db28d6693b4380bd69bb0b1b
zhennv <2094251034@qq.com>
2020-12-13 14:44:11 +0800

    创建store文件夹

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/index'
 
 import '@/assets/styles/reset.less'
 import '@/assets/styles/login.less'
@@ -18,7 +18,7 @@ Object.keys(components).forEach(item => {
   Vue.component(item, components[item]);
 })
 router.beforeEach((to, from, next) => {
-  console.log(to);
+  console.log(to, from);
   next()
   // next(vm => {
   //   console.log(vm);
